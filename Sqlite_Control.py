a9757ba919b9f1a919dc0e4c3d432437a602ae7a
JE-Chen(陳冠穎) <33644111+JE-Chen@users.noreply.github.com>
2020-07-01 04:54:36 +0800

    Update Sqlite_Control.py

--- Sqlite_Control.py
+++ Sqlite_Control.py
@@ -92,7 +92,7 @@ DATE 日期， 形如"2018-11-08"
 
 class Sqlite_Control():
 
-    def __init__(self, DB_Name='test.db'):
+    def __init__(self, DB_Name='test'):
         self.DB_Name=DB_Name
         self.connect = sqlite3.connect(DB_Name,check_same_thread=False)  # 這裡是連線上一個資料庫如果沒有這個資料庫的話就會建立一個
         self.cursor = self.connect.cursor()  # 獲取遊標cursor
