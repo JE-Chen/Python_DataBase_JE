26dd70ebc0f9adfd4421a3df1f5dacb29075d9d6
JE-Chen <zenmailman@gmail.com>
2020-12-24 09:23:01 +0800

    update & refactor
    update & refactor

--- Test/TestControl.py
+++ Test/TestControl.py
@@ -6,80 +6,42 @@ SQL.table_name = 'Account'
 
 SQL.insert_into_replace('410877027', 'test_password')
 
-SQL.insert_into_replace('410877099', 'test_password')
-
 SQL.table_name = 'LessonContent'
 
-SQL.insert_into_replace('A877', '87課', '8787878787878787878787878787878787878787', '109')
-
-SQL.insert_into_replace('A87', '87課', '8787878787878787878787878787878787878787', '109')
+SQL.insert_into_replace('A001', '測試課程', '測試內容', '109')
 
 SQL.table_name = 'LessonDetail'
 
-SQL.insert_into_replace('A877', '410877027', '87課', '99', 'HCP', '必選修', '109')
-
-SQL.insert_into_replace('A87', '410877027', '87課', '99', 'HCP', '必選修', '109')
-
-SQL.table_name = 'LessonContent'
-
-SQL.select_prefix = 'LessonDetail.LessonCode'
-
-SQL.inner_join('LessonDetail', 'LessonContent.LessonCode', 'LessonDetail.LessonCode')
+SQL.insert_into_replace('A001', '測試課程', '3', 'HCP', '必修')
 
 SQL.table_name = 'LessonGrade'
 
-SQL.insert_into_replace('A877', '410877027', '999', '109', 'HCP')
-
-SQL.insert_into_replace('A87', '410877027', '999', '109', 'HCP')
+SQL.insert_into_replace('A001', '410877027', '100', '109', 'HCP')
 
 SQL.table_name = 'PersonnelAccess'
 
 SQL.insert_into_replace('410877027', 'Super')
 
-SQL.insert_into_replace('410877099', 'Super')
-
 SQL.table_name = 'PersonnelDetail'
 
 SQL.insert_into_replace('410877027', 'JE-Chen', '107')
 
-SQL.insert_into_replace('410877099', 'JE-Chen', '107')
+SQL.table_name = 'PersonnelDetail'
+
+SQL.insert_into_replace('A001', 'JE-Chen', '109')
+
+SQL.select_prefix = "*"
 
 SQL.table_name = 'Account'
 
-SQL.select_prefix = '*'
+SQL.select_form()
 
-SQL.inner_join('LessonGrade', 'Account.PersonnelNumber', 'LessonGrade.PersonnelNumber')
+SQL.select_where('Password', 'test_password')
 
-SQL.table_name = 'LessonGrade'
+SQL.select_account('410877027', 'test_password')
 
-SQL.inner_join('LessonDetail', 'LessonGrade.LessonCode', 'LessonDetail.LessonCode')
+SQL.update('password', 'password', 'test_password', 'new_password')
 
-SQL.table_name = 'LessonDetail'
-
-# SQL.inner_join('PersonnelAccess', 'LessonDetail.PersonnelNumber', 'PersonnelAccess.PersonnelNumber')
-
-SQL.select_prefix = 'PersonnelAccess.Access'
-
-SQL.inner_join_where('PersonnelAccess', 'LessonDetail.PersonnelNumber', 'PersonnelAccess.PersonnelNumber',
-                     'PersonnelAccess.PersonnelNumber', "410877027")
-
-SQL.table_name = 'PersonnelAccess'
-
-SQL.inner_join('PersonnelDetail', 'PersonnelDetail.PersonnelNumber', 'PersonnelAccess.PersonnelNumber')
-
-SQL.table_name = 'Account'
-
-SQL.select_prefix = 'LessonContent.LessonCode'
-
-'''
-SQL.inner_inner_join(
-    'LessonDetail', 'Account.PersonnelNumber', 'LessonDetail.PersonnelNumber',
-    'LessonContent', 'LessonDetail.LessonCode', 'LessonContent.LessonCode')
-'''
-
-SQL.inner_inner_join_where(
-    'LessonDetail', 'Account.PersonnelNumber', 'LessonDetail.PersonnelNumber',
-    'LessonContent', 'LessonDetail.LessonCode', 'LessonContent.LessonCode',
-    'LessonDetail.LessonName', "87課")
+SQL.select_form()
 
 SQL.close()
