035935418bbdcfae01c19d6a56ea4153fe70a7d6
JE-Chen <zenmailman@gmail.com>
2021-03-31 21:58:28 +0800

    refactor
    refactor

--- Test/TestControl.py
+++ Test/TestControl.py
@@ -1,13 +1,11 @@
 from JEDatabase.Core.SQLiteCore import SQLiteCore
 
-SQL = SQLiteCore(db_name=r'EMPLOYEE.sqlite', table_name='Employee')
-
-SQL.table_name = 'DEPT_LOCATION'
+SQL = SQLiteCore(db_name=r'Employee.sqlite', table_name='DEPT_LOCATION')
 
 SQL.insert_into_replace('1112233554', 'TestLocation')
 
 SQL.select_prefix = "*"
 
-SQL.table_name = 'DEPT_LOCATION'
+SQL.select_form()
 
 SQL.close()
