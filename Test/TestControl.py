c1130c9e18f05812b6d1a8407fa00c5657bae91e
JE-Chen <zenmailman@gmail.com>
2021-05-21 22:58:17 +0800

    update test

--- Test/TestControl.py
+++ Test/TestControl.py
@@ -1,11 +1,20 @@
 from JEDatabase.Core.SQLiteCore import SQLiteCore
 
-SQL = SQLiteCore(db_name=r'Employee.sqlite', table_name='DEPT_LOCATION')
+SQL = SQLiteCore(db_name=r'Test.sqlite', table_name='Test')
 
-SQL.insert_into_replace('1112233554', 'TestLocation')
+SQL.create_table(
+    'CREATE TABLE IF NOT EXISTS Test(testNo VARCHAR(20) PRIMARY KEY,testData VARCHAR(20))')
+
+SQL.insert_into_ignore("000", "001")
+
+SQL.insert_into_replace("000", "002")
+
+SQL.update("testData", "testData", "003", "002")
 
 SQL.select_prefix = "*"
 
 SQL.select_form()
+
+SQL.select_where("testData", "003")
 
 SQL.close()
