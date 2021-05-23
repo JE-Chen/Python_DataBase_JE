ef9fca5514264542bc6b34de80a6c3aac0ab4b05
JE-Chen <zenmailman@gmail.com>
2021-05-23 15:10:04 +0800

    update test

--- je_database/modules/sqlite_control.py
+++ je_database/modules/sqlite_control.py
@@ -6,7 +6,7 @@ import threading
 is_import_success = True
 
 try:
-    from je_log_system.log_system import LogSystem
+    from je_log_system import LogSystem
 except ImportError:
     is_import_success = False
     print("Log is disable install je_log_system to open", file=sys.stderr)
